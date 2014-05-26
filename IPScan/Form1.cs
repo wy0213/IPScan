6c9f2dab2a30dd133feb099dc182f6cca8dabc0d
yangxin <807967316@qq.com>
2014-05-26 13:32:30 +0800

    杨昕5-26
    1、将师兄的判断设备类型的代码加进去了。
    2、增加了一个设备类型字段。
    3、修改了一下方法名

--- IPScan/Form1.cs
+++ IPScan/Form1.cs
@@ -1,23 +1,21 @@
-﻿// 名称： IPScan
-// 描述： 扫描活跃的IP地址
-// 创建人：wangying
-// 创建时间：2014-5-17
-//  做一个测试
-
-
-
-
+﻿// name： IPScan
+// decription： scan active ip
+// creator：wy yx
+// creatTime：2014-5-17
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using SnmpSharpNet;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 namespace IPScan
 {
@@ -34,7 +32,7 @@ namespace IPScan
         long startIp;
         long endIp;
         //用于存储活动IP的List
-        public List<PingIp> ipList = new List<PingIp>();
+        public List<IPMessage> ipList = new List<IPMessage>();
         //扫描主机的个数
         private int count = 0;
         //是否继续扫描,默认为继续
@@ -145,7 +143,7 @@ namespace IPScan
         {
             if (ipList != null && ipList.Count > 0)
             {
-                foreach (PingIp ip in ipList)
+                foreach (IPMessage ip in ipList)
                 {
                     if (ip.state == 1)
                     {
@@ -171,7 +169,7 @@ namespace IPScan
         private void scan()
         {
             //重新扫描的时候假设其他人已经下线
-            foreach (PingIp ip in ipList)
+            foreach (IPMessage ip in ipList)
             {
                 ip.state = 0;
             }
@@ -185,10 +183,10 @@ namespace IPScan
                 //调用IpTrs（）函数，将X转换成IP地址
                 string ip = IpTrs(x);
 
-                #region 使用Ping扫描ip
-                PingIp IP = new PingIp();
+                #region 为每个IP扫描器创建一个线程
+                IPMessage IP = new IPMessage();
                 //使用线程会出现有的ip扫描了几遍，有的ip没有扫描 )
-                creatPingThread = new Thread(IP.pingScan);
+                creatPingThread = new Thread(IP.ScanIP);
                 IP.IpAds = ip;
                 IP.form = this;
                 creatPingThread.IsBackground = true;
@@ -257,7 +255,7 @@ namespace IPScan
 
     }
     //此类用于线程调用时，存数据
-    public class PingIp
+    public class IPMessage
     {
         #region 导入ARP_API
         [DllImport("iphlpapi.dll")]
@@ -266,37 +264,28 @@ namespace IPScan
         static extern Int32 inet_addr(string ipaddr);
         private const Int32 NUMBER_OF_PHYSICAL_ADDRESS_BYTES = 6;
         #endregion
-
         //IP地址
         public string IpAds;
         //物理地址
         public string macDest;
-        //是否是活动主机（默认为否）
+        //是否是活动主机（0-不是活动 1-活动主机）
         public int state = 0;
+        //设备类型(0-未识别 1-电脑主机 2-路由器...)默认为主机
+        public int equipType = 1;
         //引用控件类
         public Form1 form;
 
-        public PingIp() { }
-        public void pingScan() //设备地址扫描
+        public IPMessage() { }
+        public void ScanIP() //设备地址扫描
         {
-            try
+            arpScan();
+            pingScan();
+            if (this.state == 1)
             {
-                Ping ping = new Ping();
-                PingOptions op = new PingOptions();
-                op.DontFragment = true;
-                int timeout = 2000;
-                string data = "aaa";
-                byte[] buff = Encoding.ASCII.GetBytes(data);
-                PingReply reply = ping.Send(IpAds, timeout, buff, op);
-                if (reply.Status == IPStatus.Success)
-                {
-                    this.state = 1;
-                    form.ipList.Add(this);
-                }
-                arpScan();
+                judgeEquipType();
             }
-            catch (Exception ex) { }
         }
+        //使用arp协议扫描IP
         public void arpScan()
         {
             try
@@ -330,11 +319,283 @@ namespace IPScan
                 }
                 
             }
-            catch
+            catch{ }
+        }
+        //使用ping扫描IP
+        public void pingScan()
+        {
+            try
             {
-                
+                Ping ping = new Ping();
+                PingOptions op = new PingOptions();
+                op.DontFragment = true;
+                int timeout = 2000;
+                string data = "aaa";
+                byte[] buff = Encoding.ASCII.GetBytes(data);
+                PingReply reply = ping.Send(IpAds, timeout, buff, op);
+                if (reply.Status == IPStatus.Success)
+                {
+                    this.state = 1;
+                    form.ipList.Add(this);
+                }
+            }
+            catch { }
+        }
+        //判断设备类型
+        public void judgeEquipType()
+        {
+            string hoststr = this.IpAds;
+            string comstr = "public";
+            byte[] resstr = new byte[1024];
+            SNMP msg = new SNMP();
+            //1.3.6.1.2.1.1.7.0……设备的网络功能（主机、路由器等）
+            resstr = msg.getMessage("get", hoststr, comstr, "1.3.6.1.2.1.1.7.0");
+            if (resstr[0] == 0xff)//没有收到来自host的消息，对方没有开启snmp服务
+            {
+                //MessageBox.Show("没有收到来自 " + hoststr + " 的消息。");
+                return;
+            }
+            int comleng = Convert.ToInt16(resstr[6]);
+            int mibleng = Convert.ToInt16(resstr[23 + comleng]);
+            int datatype = Convert.ToInt16(resstr[24 + comleng + mibleng]);
+            int dataleng = Convert.ToInt16(resstr[25 + comleng + mibleng]);
+            int start = 26 + comleng + mibleng;
+            //string outprint = Encoding.ASCII.GetString(resstr, start, dataleng);
+            string outprint = BitConverter.ToString(resstr, start, dataleng);
+            int num = Int32.Parse(outprint, System.Globalization.NumberStyles.HexNumber);
+            if (num == 76 || num == 72)//主机
+            {
+                this.equipType = 1;
+            }
+            else if (num == 4)//路由器
+            {
+                this.equipType = 2;
+            }
+            else//无法识别
+            {
+                this.equipType = 0;
             }
 
         }
     }
+
+    #region SNMP类
+    class SNMP
+    {
+        public SNMP()
+        {
+        }
+        public byte[] getMessage(string request, string hostString, string community, string mibStr)
+        {
+            //初始化发送的SNMP包数组
+            byte[] sendPacket = new byte[1024];
+            //初始化返回的SNMP包数组
+            byte[] returnPacket = new byte[1024];
+            //初始化存放MIB对象的数组
+            byte[] mib = new byte[1024];
+            //将mib字符串数组用"."分隔开 并存入mibvalue字符串数组
+            string[] mibvalue = mibStr.Split('.');
+
+            //获得mibvalue字符串的长度 作为mib消息的长度
+            int miblength = mibvalue.Length;
+            //当前的mib长度
+            int orgmiblength = miblength;
+            //获得团体名长度
+            int comlength = community.Length;
+
+            int current = 0, ps = 0, temp, i;
+            //初始化SNMP包的长度
+            int snmplength;
+
+            //这段for循环 将mib字符串数组转换为byte类型 存入mib byte数组中
+            for (i = 0; i < orgmiblength; i++)
+            {
+                //首先将字符串转换为16位Int型
+                temp = Convert.ToInt16(mibvalue[i]);
+                if (temp > 127)
+                {
+                    //若mib中对象的标识符大于128 则将其高位存在前面的数组中
+                    mib[current] = Convert.ToByte(128 + (temp / 128));
+                    mib[current + 1] = Convert.ToByte(temp - ((temp / 128) * 128));
+                    //由于数字占了两位 下标加2
+                    current = current + 2;
+                    //mib长度只加1
+                    miblength++;
+                }
+                else
+                {
+                    //若标识符小于128则直接转化为byte类型存入
+                    mib[current] = Convert.ToByte(temp);
+                    current++;
+                }
+            }
+
+            //计算出SNMP包总长
+            snmplength = 29 + comlength + miblength - 1;
+            //开始填写SNMP包的版本
+            //16进制数0x30代表传输开始
+            sendPacket[ps++] = 0x30;
+            //填写SNMP包的长度 为SNMP包的总长度减去传输开始和长度这2位
+            sendPacket[ps++] = Convert.ToByte(snmplength - 2);
+
+            //填写SNMP版本
+            //0x02说明类型为整数型
+            sendPacket[ps++] = 0x02;
+            //0x01说明长度为1
+            sendPacket[ps++] = 0x01;
+            //0x00说明版本号为0
+            sendPacket[ps++] = 0x00;
+
+            //填写团体名
+            //0x04说明类型为字符串型
+            sendPacket[ps++] = 0x04;
+            //将团体长度转换为byte类型并填入数组
+            sendPacket[ps++] = Convert.ToByte(comlength);
+
+            //将团体名转化为byte类型存入snmpData数组中
+            byte[] snmpData = Encoding.ASCII.GetBytes(community);
+            for (i = 0; i < snmpData.Length; i++)
+            {
+                //将byte类型 存入SNMP包中
+                sendPacket[ps++] = snmpData[i];
+            }
+
+            //填写SNMP PDU
+            //设置SNMP的PDU类型 若PDU信息为get 
+            //填入A0表示 Request 格式
+            if ("get" == request)
+                sendPacket[ps++] = 0xA0;
+            //填入A1表示 NextRequest 格式
+            else
+                sendPacket[ps++] = 0xA1;
+            //填入PDU的数据长度
+            sendPacket[ps++] = Convert.ToByte(20 + miblength - 1);
+
+            //填写PDU请求ID
+            //填写数据类型为整型
+            sendPacket[ps++] = 0x02;
+            //填写长度为4
+            sendPacket[ps++] = 0x04;
+            //填写请求ID
+            sendPacket[ps++] = 0x00;
+            sendPacket[ps++] = 0x00;
+            sendPacket[ps++] = 0x00;
+            sendPacket[ps++] = 0x01;
+
+            //填写错误状态信息
+            //填写整型类型
+            sendPacket[ps++] = 0x02;
+            //填写错误长度类型
+            sendPacket[ps++] = 0x01;
+            //填写错误状态类型
+            sendPacket[ps++] = 0x00;
+
+            //填写错误索引
+            //填写整型类型
+            sendPacket[ps++] = 0x02;
+            //填写长度类型
+            sendPacket[ps++] = 0x01;
+            //填写错误状态类型
+            sendPacket[ps++] = 0x00;
+
+            //开始绑定变量
+            //0x30说明变量绑定开始
+            sendPacket[ps++] = 0x30;
+            //填写变量长度
+            sendPacket[ps++] = Convert.ToByte(6 + miblength - 1);
+            //0x30开始填写第一个变量
+            sendPacket[ps++] = 0x30;
+            //填写变量长度
+            sendPacket[ps++] = Convert.ToByte(6 + miblength - 1 - 2);
+
+            //下面填写MIB类型
+            //说明变量类型为对象 
+            sendPacket[ps++] = 0x06;
+            //填写对象类型
+            sendPacket[ps++] = Convert.ToByte(miblength - 1);
+            //MIB开始填写
+            //用0x2b代表MIB对象的标识符1.3
+            sendPacket[ps++] = 0x2B;
+            //填写mib对象
+            for (i = 2; i < miblength; i++)
+                //根据长度开始填写MIB对象
+                sendPacket[ps++] = Convert.ToByte(mib[i]);
+
+            //填写最后结束类型为空
+            sendPacket[ps++] = 0x05;
+            //填写长度为0
+            sendPacket[ps++] = 0x00;
+
+            //下面编写发送程序
+            //初始化Socket对象
+            Socket sendSock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //设置Socket的超时时间为5秒
+            sendSock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 5000);
+            //使用传入的IP字符串来初始化为IPAddress 类型的IP地址
+            IPAddress hostip = IPAddress.Parse(hostString);
+            //从端口161发送信息
+            IPEndPoint iep = new IPEndPoint(hostip, 161);
+            EndPoint ep = (EndPoint)iep;
+
+            try
+            {
+                //开始发送套接字
+                sendSock.SendTo(sendPacket, snmplength, SocketFlags.None, iep);
+            }
+            catch (Exception e)
+            {
+                //若发送失败 弹出出错消息
+                MessageBox.Show("与服务器连接失败");
+                //把SNMP包的首字符改为 0xff 说明传送失败
+                returnPacket[0] = 0xff;
+                //传回SNMP包
+                return returnPacket;
+            }
+
+            //若发送成功 
+            try
+            {
+                //接收返回消息
+                int recv = sendSock.ReceiveFrom(returnPacket, ref ep);
+            }
+            catch (SocketException)
+            {
+                //若接收失败 修改SNMP包首字符为0xff
+                returnPacket[0] = 0xff;
+            }
+            //返回收到的SNMP包
+            return returnPacket;
+        }
+
+        //将byte数组mibName转换为string类型
+        public string nextMIBMessage(byte[] mibName)
+        {
+            //字符串首字符为"1.3"
+            string outStr = "1.3";
+            int cmutyleng = mibName[6];
+            int start = 6 + cmutyleng + 17;
+            int mibleng = mibName[start] - 1;
+            //除去字符“1.3” 标志start加2
+            start += 2;
+            int mibVal;
+
+            //使用for循环来将byte类型的mib标识符转化为字符串类型
+            for (int i = start; i < start + mibleng; i++)
+            {
+                mibVal = Convert.ToInt16(mibName[i]);
+                //若欲转换字符大于128 
+                if (mibVal > 128)
+                {
+                    //则将高位先行放入字符串
+                    mibVal = (mibVal / 128) * 128 + Convert.ToInt16(mibName[i + 1]);
+                    i++;
+                }
+                //将每个字符加入MIB字符串outStr中
+                outStr += "." + mibVal;
+            }
+            //将转换好的MIB字符串作为返回值返回
+            return outStr;
+        }
+    }
+    #endregion 
 }
